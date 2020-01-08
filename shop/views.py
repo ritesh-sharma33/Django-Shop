134e4a3a7e6579bb5d1ad6d62f8be8c5ca11c1e5
Ritesh Sharma <sharmaritesh3312@gmail.com>
2020-01-08 18:59:42 +0530

    Products Logic Implemented

--- shop/views.py
+++ shop/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Product
+from math import ceil
 
 # Create your views here.
 def index(request):
-  return render(request, 'shop/index.html')
+  products = Product.objects.all()
+  print(products)
+  n = len(products)
+  nSlides = n // 4 + ceil((n / 4) + (n // 4))
+  params = {'no_of_slides': nSlides, 'range': range(1, nSlides), 'product': products}
+
+  return render(request, 'shop/index.html', params)
 
 def about(request):
   return render(request, 'shop/about.html')
